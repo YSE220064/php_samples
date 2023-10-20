a3c821e457a3ae23495302f1f096cbe5d13ca55f
220064@YSE <220064md@yse-c.net>
2023-10-20 11:18:02 +0900

    fetchpull

--- regist/confirm.php
+++ regist/confirm.php
@@ -3,16 +3,39 @@
 session_start();
 session_regenerate_id(true);
 
-// PHP program
-$regist = $_POST;
+// POSTリクエストのデータを 変数 regist に代入
+$regist = check($_POST);
 
 //TODO: データチェック
+// パスワードが8文字以上、20文字以内でなければ、入力画面にリダイレクト
+if (
+    mb_strlen($regist['password']) < 8 ||
+    mb_strlen($regist['password']) > 20
+) {
+    $_SESSION['errors']['password'] = "パスワードは８文字以上、20文字以内で入力してください";
+    header('Location: input.php');
+}
 
 // セッション保存
 $_SESSION['regist'] = $regist;
 
 $genders['male'] = "男性";
 $genders['female'] = "女性";
+
+
+/**
+ * サニタイズ
+ */
+function check($posts)
+{
+    if (empty($posts)) return;
+    foreach ($posts as
+        $column => $post) {
+        $posts[$column] =
+            htmlspecialchars($post, ENT_QUOTES, 'UTF-8');
+    }
+    return $posts;
+}
 ?>
 
 <!DOCTYPE html>
@@ -26,27 +49,29 @@ $genders['female'] = "女性";
     <div class="container">
         <h1>確認画面</h1>
         <p>この内容で登録してもよろしいですか？</p>
-        <form action="store.php" method="post">
-            <div>
-                <label for="">氏名</label>
-                <?= $regist['name'] ?>
-            </div>
-            <div>
-                <label for="">Email</label>
-                <?= $regist['email'] ?>
-            </div>
-            <div>
-                <label for="">生年月日</label>
-                <?= $regist['year'] ?>/<?= $regist['month'] ?>/<?= $regist['day'] ?>
-            </div>
-            <div>
-                <label for="">性別</label>
-                <?= $genders[$regist['gender']] ?>
-            </div>
-            <button class="btn btn-primary">登録</button>
+        <div>
+            <form action="store.php" method="post">
+                <div>
+                    <label for="">氏名</label>
+                    <?= $regist['name'] ?>
+                </div>
+                <div>
+                    <label for="">Email</label>
+                    <?= $regist['email'] ?>
+                </div>
+                <div>
+                    <label for="">生年月日</label>
+                    <?= $regist['year'] ?>/<?= $regist['month'] ?>/<?= $regist['day'] ?>
+                </div>
+                <div>
+                    <label for="">性別</label>
+                    <?= $genders[$regist['gender']] ?>
+                </div>
+                <button class="btn btn-primary">登録</button>
 
-            <a class="btn btn-outline-primary" href="input.php">戻る</a>
-        </form>
+                <a class="btn btn-outline-primary" href="input.php">戻る</a>
+            </form>
+        </div>
     </div>
 </body>
 
--- regist/input.php
+++ regist/input.php
@@ -6,10 +6,28 @@ session_regenerate_id(true);
 if (isset($_SESSION['regist'])) {
     $regist = $_SESSION['regist'];
 }
+if (isset($_SESSION['errors'])) {
+    $errors = $_SESSION['errors'];
+}
+
+$genders['male'] = "男性";
+$genders['female'] = "女性";
+
+function checked($value, $checkValue)
+{
+    return ($value == $checkValue) ? 'checked' : '';
+}
+
+function selected($value, $checkValue)
+{
+    return ($value == $checkValue) ? 'selected' : '';
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- 1つ上の階層の componentsフォルダの head.php を読み込む -->
 <?php include('../components/head.php'); ?>
 
 <body>
@@ -17,6 +35,12 @@ if (isset($_SESSION['regist'])) {
 
     <div class="container">
         <h1>入力画面</h1>
+
+        <ul>
+            <?php foreach ($errors as $error) : ?>
+                <li class="text-danger"><?= $error ?></li>
+            <?php endforeach ?>
+        </ul>
         <form action="confirm.php" method="post">
             <div class="form-group">
                 <label class="form-label" for="">氏名</label>
@@ -34,26 +58,30 @@ if (isset($_SESSION['regist'])) {
                 <label class="form-label" for="">生年月日</label>
                 <select class="form-control" name="year">
                     <?php foreach (range(1900, date('Y')) as $year) : ?>
-                        <option value="<?= $year ?>"><?= $year ?></option>
+                        <option value="<?= $year ?>" <?= selected($year, $regist['year']) ?>><?= $year ?></option>
                     <?php endforeach ?>
                 </select>
                 <select class="form-control" name="month">
                     <?php foreach (range(1, 12) as $month) : ?>
-                        <option value="<?= $month ?>"><?= $month ?></option>
+                        <option value="<?= $month ?>" <?= selected($month, $regist['month']) ?>><?= $month ?></option>
                     <?php endforeach ?>
                 </select>
                 <select class="form-control" name="day">
                     <?php foreach (range(1, 31) as $day) : ?>
-                        <option value="<?= $day ?>"><?= $day ?></option>
+                        <option value="<?= $day ?>" <?= selected($day, $regist['day']) ?>><?= $day ?></option>
                     <?php endforeach ?>
                 </select>
             </div>
             <div>
                 <label class="form-label" for="">性別</label>
-                <input id="male" type="radio" name="gender" value="male">
-                <label for="male">Male</label>
-                <input id="female" type="radio" name="gender" value="female">
-                <label for="female">Female</label>
+                <div class="form-check">
+                    <input id="male" type="radio" name="gender" value="male" <?= checked('male', @$regist['gender']) ?>>
+                    <label for="male"><?= $genders['male'] ?></label>
+                </div>
+                <div class="form-check">
+                    <input id="female" type="radio" name="gender" value="female" <?= checked('female', @$regist['gender']) ?>>
+                    <label for="female"><?= $genders['female'] ?></label>
+                </div>
             </div>
             <button class="btn btn-primary">確認</button>
         </form>
