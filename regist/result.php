8c3b10e8c161e2fb43fbc80378126f254f60d3ef
220064@YSE <220064md@yse-c.net>
2023-10-20 09:20:33 +0900

    first commit

---
+++ regist/result.php
@@ -0,0 +1,18 @@
+<?php
+//TODO：セッションチェック
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('../components/head.php'); ?>
+
+<body>
+    <?php include('../components/nav.php'); ?>
+
+    <h2>会員登録完了</h2>
+    <p>
+        登録ありがとうございました。
+    </p>
+</body>
+
+</html>
